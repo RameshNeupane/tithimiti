258f3514337cac5fc8da4376647275996045d441
RameshNeupane <neupaneramesh555@gmail.com>
2022-02-08 10:30:49 +0545

    add Calendar render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Calendar from "./components/Calendar/Calendar";
 import Today from "./components/Today/Today";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <h1 className="title">tithimiti</h1>
       <Today />
+      <Calendar />
     </div>
   );
 }
